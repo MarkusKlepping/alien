b70267a19e6e09c0e3ef8331aaf1476be8a7e5e0
MarkusKlepping <codeworkspace@MacBook-Pro-von-Markus.local>
2021-10-27 19:12:51 +0200

    tried and failed to display images in pygame :)

--- ship.py
+++ ship.py
@@ -5,11 +5,11 @@ class Ship:
 
   def __init__(self, ai_game):
     """Initialize the ship and set its starting positioin."""
-    self.screen = ai.game.screen
+    self.screen = ai_game.screen
     self.screen_rect = ai_game.screen.get_rect()
 
     #load the ship image and get its rect.
-    self.image = pygame.image.load("images/ship.jpeg")
+    self.image = pygame.image.load("images/ship.bmp")
     self.rect = self.image.get_rect()
 
     #Start each new ship at the bottom center of the screen
